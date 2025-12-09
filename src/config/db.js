5b2d6aa1dd2a9abed1f52d8ed879bb856535a7f1
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2025-12-09 20:47:38 -0300

    Conected with the Data Base

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  connectionLimit: process.env.MYSQL_CONN_LIMIT,
+});
+
+module.exports = pool;
---
+++ src/server.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const app = express()
+
+app.use(express.json())
+
+app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"))
